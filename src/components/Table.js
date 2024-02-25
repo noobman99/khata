67bbc118f700a803dcff77f63432f4267e510b23
Parth Patil <psp90804@gmail.com>
2024-02-25 16:45:30 +0530

    Transaction page mobile view

--- src/components/Table.js
+++ src/components/Table.js
@@ -2,14 +2,18 @@ import { Link } from "react-router-dom";
 import "../css/Table.css";
 import useCoreDataContext from "../Hooks/useCoreDataContext";
 import { toast } from "react-toastify";
+import { useState } from "react";
 
 const TableHeader = (props) => {
   return (
     <thead>
       <tr>
-        {props.headers.map((header, index) => (
-          <th key={index}>{header}</th>
-        ))}
+        <th>Date</th>
+        <th>Reason</th>
+        <th>Amount</th>
+        <th className="smscr-only"></th>
+        <th className="bigscr-only">Category</th>
+        <th className="bigscr-only">Controls</th>
       </tr>
     </thead>
   );
@@ -18,6 +22,11 @@ const TableHeader = (props) => {
 const TableRow = (props) => {
   const API_URL =
     process.env.REACT_APP_BACKEND + "/transactions/" + props.data.rowid;
+
+  const [expanded, setExpanded] = useState(false);
+  const toggleCollapse = () => {
+    setExpanded(!expanded);
+  };
 
   let { user, dispatch } = useCoreDataContext();
   const onDel = (rowid) => {
@@ -58,20 +67,53 @@ const TableRow = (props) => {
   };
 
   return (
-    <tr>
-      <td>{props.data.date}</td>
-      <td>{props.data.reason}</td>
-      <td>{props.data.amount}</td>
-      <td>{props.data.category}</td>
-      <td className="td-controls">
-        <button className="table-control-button" onClick={deleteTransaction}>
-          <i className="fa-regular fa-trash-can" />
-        </button>
-        <Link to={"/edit/" + props.data.rowid} className="table-control-button">
-          <i className="fa-solid fa-pen" />
-        </Link>
-      </td>
-    </tr>
+    <>
+      <tr className={props.className} onClick={toggleCollapse}>
+        <td>{props.data.date}</td>
+        <td>{props.data.reason}</td>
+        <td>{props.data.amount}</td>
+        <td className="smscr-only">
+          <i
+            className={
+              "fa-sharp fa-solid fa-caret-down downarrow" +
+              (expanded ? " inverted" : "")
+            }
+          />
+        </td>
+        <td className="bigscr-only">{props.data.category}</td>
+        <td className="td-controls bigscr-only">
+          <button className="table-control-button" onClick={deleteTransaction}>
+            <i className="fa-regular fa-trash-can" />
+          </button>
+          <Link
+            to={"/edit/" + props.data.rowid}
+            className="table-control-button"
+          >
+            <i className="fa-solid fa-pen" />
+          </Link>
+        </td>
+      </tr>
+      <tr
+        className={props.className + " smscr-only"}
+        style={{ visibility: expanded ? "visible" : "collapse" }}
+      >
+        <td>
+          <strong>Category :</strong>
+        </td>
+        <td>{props.data.category}</td>
+        <td colSpan={2} className="td-controls">
+          <button className="table-control-button" onClick={deleteTransaction}>
+            <i className="fa-regular fa-trash-can" />
+          </button>
+          <Link
+            to={"/edit/" + props.data.rowid}
+            className="table-control-button"
+          >
+            <i className="fa-solid fa-pen" />
+          </Link>
+        </td>
+      </tr>
+    </>
   );
 };
 
@@ -87,7 +129,12 @@ export default function Table(props) {
             return d - c;
           })
           .map((row, index) => (
-            <TableRow key={index} data={row} onDel={props.onDel} />
+            <TableRow
+              key={index}
+              data={row}
+              onDel={props.onDel}
+              className={index % 2 ? "odd" : "even"}
+            />
           ))}
       </tbody>
     </table>
